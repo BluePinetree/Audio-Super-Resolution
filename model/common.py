7c628c8dd8ef7c35f30bda26b1f7fa7b09fd66b4
김윤수 <33441881+BluePinetree@users.noreply.github.com>
2019-11-19 02:10:44 +0900

    Fixed normalize, denormalize and snr metric function

--- model/common.py
+++ model/common.py
@@ -3,26 +3,36 @@ import tensorflow as tf
 def resolve(model, batch):
     lr_batch = tf.cast(batch, tf.dtypes.float32)
     sr_batch = model(lr_batch)
-    sr_batch = denormalize(sr_batch)
     return sr_batch
 
 def evaluate(model, dataset):
     snr_values = []
     for lr, hr in dataset:
-        lr, hr = tf.reshape(lr, [-1, lr.shape[1], 1]), tf.reshape(hr, [-1, hr.shape[1], 1])
+        # 전처리
+        lr, hr = tf.reshape(lr, (-1, lr.shape[0], 1)), tf.reshape(hr, (-1, hr.shape[0], 1))
+        #평가
         sr = model(lr)
         snr_value = snr(hr, sr)
         snr_values.append(snr_value)
-    return tf.reduce_mean(snr_values, axis=0)[0]
-
+    return tf.reduce_mean(snr_values)
 
 def normalize(x):
     x = x / 2**15
-    return tf.cast(x, tf.dtypes.float32)
+    return x
+
+def normalize_1(x):
+    x = x + 2**15
+    x = x / 2**16
+    return x
 
 def denormalize(x):
     x = x * (2**15)
-    return tf.cast(x, tf.dtypes.int16)
+    return x
+
+def denormalize_1(x):
+    x = x * (2**16)
+    x = x - 2**15
+    return x
 
 # ---------------------------------------
 #  Metric
@@ -30,7 +40,9 @@ def denormalize(x):
 def snr(hr, sr):
     signal = tf.reduce_sum(hr ** 2)
     noise = tf.reduce_sum((hr - sr) ** 2)
-    return 20*tf.math.log(signal/noise, name='SNR')
+    numerator = tf.math.log(signal/noise)
+    denorminator = tf.math.log(tf.constant(10, dtype=numerator.dtype))
+    return 10*(numerator/denorminator)
 
 # ---------------------------------------
 #  See https://arxiv.org/abs/1609.05158
